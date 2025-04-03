3f6f0b69c48ac666a36f13b57c63780b0e55c717
alessiomolinas <alessio.molinas@gmail.com>
2025-04-03 23:14:57 +0200

    lab01_esercizio2

--- laboratori/lab01.sql
+++ laboratori/lab01.sql
@@ -1,8 +1,13 @@
---Gruppo Botto Ugo, Molinas Alessio, Romano Ettore
+-- ####################################################################
+--  Gruppo Botto Ugo, Molinas Alessio, Romano Ettore
+-- #####################################################################
+-- ESERCIZIO 1
+
+---- ESERCIZIO 1.0
 CREATE SCHEMA corsi;
 set search_path to corsi;
 
---PUNTO UNO
+---- ESERCIZIO 1.1
 CREATE TABLE Professori
 	(Id DECIMAL(5) PRIMARY KEY,
 	 Cognome VARCHAR(20) NOT NULL,
@@ -37,7 +42,7 @@ INSERT INTO Professori VALUES (54661, 'camminata','alessio', 123456.78, FALSE); 
 
 INSERT INTO Professori VALUES (54664, 'camminata','alessio',10000,FALSE);
 
---SECONDA PARTE
+---- ESERCIZIO 1.2
 CREATE TABLE Corsi
 	(Id CHAR(10) PRIMARY KEY,
 	 CorsoDiLaurea VARCHAR(20) NOT NULL,
@@ -51,16 +56,54 @@ INSERT INTO Corsi VALUES ('BASDAT1_25','Informatica','BASI DI DATI 1', 54661,TRU
 INSERT INTO Corsi VALUES ('BASDAT2_25','Informatica','BASI DI DATI 2', 54662,TRUE);
 --INSERT INTO Corsi VALUES ('ALGEBR2025','Informatica','ALGEBRA', 54664,TRUE);
 --INSERT INTO Corsi VALUES ('ALGLOG2025','Informatica','ALGEBRA & LOGICA');
-
---TEST INSERIMENTO
 SELECT * FROM Corsi;
 
--- TEST 1
+-- TEST
 UPDATE Professori SET Id = 64661 WHERE id = 54661;
 SELECT * FROM  Corsi where Professori_Id=64661;
 -- Update eseguito
 
--- TEST 2
+-- TEST
 DELETE FROM Professori WHERE id = 64661;
 -- ForeignKeyViolation: update or delete on table "professori" violates foreign key constraint "corsi_professori_id_fkey" on table "corsi"
 --DETAIL:  Key (id)=(64661) is still referenced from table "corsi".
+
+
+
+-- ##################################################################################################################
+-- ESERCIZIO 2 - Ulteriori attività di laboratorio su SQL DDL (se volete esercitarvi ulteriormente su questa parte)
+-- ##################################################################################################################
+---- ESERCIZIO 2.0 Posizionarsi nello schema "corsi" creato nell'esercitazione di laboratorio eseguendo set search_path to “corsi”
+set search_path to corsi;
+
+---- ESERCIZIO 2.1 Creare la relazione che mantiene i dati degli studenti dell'Università di Genova
+CREATE TABLE Studenti
+	(Matricola SERIAL PRIMARY KEY,
+	 Cognome VARCHAR(20) NOT NULL,
+	 Nome VARCHAR(20) NOT NULL,
+	 CorsoDiLaurea VARCHAR(20) NOT NULL,
+	 Iscrizione CHAR(9) NOT NULL, 
+	 Relatore DECIMAL(5),
+	 FOREIGN KEY (Relatore) REFERENCES Professori ON UPDATE CASCADE ON DELETE NO ACTION
+	);
+INSERT INTO Studenti VALUES(DEFAULT,'MOLINAS','ALESSIO','INFORMATICA','2022/2023');
+INSERT INTO Studenti VALUES(DEFAULT,'BOTTO','UGO','INFORMATICA','2021/2022');
+INSERT INTO Studenti VALUES(DEFAULT,'ROMANO','ETTORE','INFORMATICA','2021/2022');
+
+INSERT INTO Studenti VALUES(DEFAULT,'ROSSI','GIANFRANCO','INFORMATICA','2021/2022',5512);
+-- ForeignKeyViolation: insert or update on table "studenti" violates foreign key constraint "studenti_relatore_fkey"
+-- DETAIL:  Key (relatore)=(5512) is not present in table "professori".
+
+INSERT INTO Studenti VALUES(DEFAULT,'ROSSI','GIANFRANCO','INFORMATICA','2021/2022',64661);
+-- TEST 1
+UPDATE Professori SET Id = 54660 WHERE id = 64661;
+SELECT * FROM  Studenti where Relatore=54660;
+-- TEST 2
+DELETE FROM Professori WHERE id = 54660;
+-- ForeignKeyViolation: update or delete on table "professori" violates foreign key constraint "corsi_professori_id_fkey" on table "corsi"
+--DETAIL:  Key (id)=(54660) is still referenced from table "corsi".
+
+-- 4 Provare a modificare le tabelle definite, in particolare provare a
+---- 4.1 Modificare la tabella Corsi aggiungendo un attributo opzionale MutuaDa alfanumerico di 10 caratteri che costituisce una chiave esterna su Corsi e rappresenta l’identificatore del corso da cui il corso è eventualmente mutuato. Specificare il comportamento che si ritiene più opportuno in caso di violazioni dell’integrità referenziale. Inserire una nuova tupla corrispondente al corso di Basi di dati per Smid che mutua da Basi di dati per Informatica.
+
+ALTER TABLE Corsi ADD column MutuaDa TYPE CHAR(10) REFERENCES Corsi ON UPDATE CASCADE ON DELETE NO ACTION;
