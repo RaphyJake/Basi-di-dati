1a3329d1320b4cc408ceb507292ef6978d4a8276
alessiomolinas <alessio.molinas@gmail.com>
2025-04-10 16:18:33 +0200

    lab02 parte obbligatoria

--- laboratori/lab02.sql
+++ laboratori/lab02.sql
@@ -17,3 +17,54 @@ SELECT matricola, cognome, nome FROM Studenti WHERE Residenza IN ('Genova','La S
 ---------------------------------------------
 -- Interrogazioni su più relazioni         --
 ---------------------------------------------
+/**** 1. la matricola degli studenti laureatisi in informatica prima del novembre 2009; ****/
+set search_path to 'unicorsi';
+SELECT Matricola FROM Studenti JOIN CorsiDiLaurea ON CorsoDiLaurea = CorsiDiLaurea.Id where Laurea < '01-11-2009' and Denominazione = 'Informatica'
+
+/**** 2. l’elenco in ordine alfabetico dei nominativi degli studenti, con, per ognuno, il cognome del relatore associato; ****/
+set search_path to 'unicorsi';
+SELECT s.Cognome, s.Nome, p.Cognome as Relatore FROM Studenti s JOIN Professori p ON s.Relatore = p.Id order by s.Cognome, s.Nome
+
+/**** 3. l’elenco, senza duplicati e in ordine alfabetico inverso, degli studenti che hanno presentato il piano di studi per il quinto anno del corso di laurea di informatica nell’a.a. 2011/2012 e sono in tesi (hanno assegnato un relatore). ****/
+set search_path to 'unicorsi';
+SELECT DISTINCT s.Cognome, s.Nome
+FROM Studenti s
+JOIN CorsiDiLaurea cdl ON s.CorsoDiLaurea  = cdl.id  
+JOIN PianiDiStudio pds ON s.Matricola  = pds.Studente
+WHERE 	cdl.Denominazione = 'Informatica'
+		AND pds.Anno = 5
+		AND AnnoAccademico = '2011'
+		AND s.Relatore IS NOT NULL
+ORDER BY Cognome DESC, Nome DESC
+
+---------------------------------------------
+-- Operazioni insiemistiche                --
+---------------------------------------------
+
+/**** 1.cognome, nome e qualifica (’studente’/’professore’) di studenti e professori. --- senza ordinamento mette male confrontarli con i risultati (eseguito check su Excel ****/
+set search_path to 'unicorsi';
+SELECT Nome, Cognome, 'studente' FROM Studenti
+UNION ALL
+Select Nome, Cognome, 'professore' FROM Professori;
+
+/***** Ho scritto alla proff per chiarimenti *****/
+/**** 2. gli studenti di informatica che hanno passato basi di dati 1 ma non interfacce grafiche nel giugno del 2010.  ****/
+set search_path to 'unicorsi';
+
+SELECT s.Matricola FROM Studenti s
+JOIN CorsiDiLaurea cdl ON s.CorsoDiLaurea = cdl.id
+WHERE cdl.Denominazione = 'Informatica'
+AND s.Matricola IN (SELECT e.studente FROM Esami e JOIN Corsi c ON e.corso = c.id
+WHERE e.Data >='2010-06-01' and e.Data <='2010-06-30' and c.Denominazione = 'Basi Di Dati 1' and e.voto >=18)
+AND s.Matricola IN (SELECT e.studente FROM Esami e JOIN Corsi c ON e.corso = c.id
+WHERE e.Data >='2010-06-01' and e.Data <='2010-06-30' and c.Denominazione = 'Interfacce Grafiche' and e.voto <18)
+
+/**** 3. gli studenti di informatica che hanno passato sia basi di dati 1 che interfacce grafiche nel giugno del 2010. ****/
+set search_path to 'unicorsi';
+SELECT s.Matricola FROM Studenti s
+JOIN CorsiDiLaurea cdl ON s.CorsoDiLaurea = cdl.id
+WHERE cdl.Denominazione = 'Informatica'
+AND s.Matricola IN (SELECT e.studente FROM Esami e JOIN Corsi c ON e.corso = c.id
+WHERE e.Data >='2010-06-01' and e.Data <='2010-06-30' and c.Denominazione = 'Basi Di Dati 1' and e.voto >=18)
+AND s.Matricola IN (SELECT e.studente FROM Esami e JOIN Corsi c ON e.corso = c.id
+WHERE e.Data >='2010-06-01' and e.Data <='2010-06-30' and c.Denominazione = 'Interfacce Grafiche' and e.voto >=18)
