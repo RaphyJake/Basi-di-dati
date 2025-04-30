307650b2bd896b6419d1fae0c928e3445e4f7bfa
alessiomolinas <alessio.molinas@gmail.com>
2025-04-30 22:30:53 +0200

    fine lab

--- laboratori/lab04.sql
+++ laboratori/lab04.sql
@@ -5,25 +5,34 @@ set search_path to "unicorsi";
 
 -- SOTTOINTERROGAZIONI
 /**** 1. (*) [questa interrogazione corrisponde a un'interrogazione già formulata in esercitazioni precedenti (con INTERSECT e IN), la richiesta è ora di formularla  IN DUE DIVERSI MODI: usando EXISTS e una sotto-interrogazione correlata E senza usare sotto-interrogazioni ma usando due alias sulle relazioni] la matricola degli studenti di informatica che nel mese di giugno 2010 hanno registrato voti sia per il corso di basi di dati 1 che per quello di interfacce grafiche; ****/
-
 --- VERSIONE 1
-SELECT DISTINCT e1.Studente FROM Esami e1 
+
+SELECT DISTINCT e1.Studente
+FROM Esami e1 
 JOIN Corsi c1 ON e1.Corso = c1.id
-WHERE e1.Voto>=18 AND e1.Data BETWEEN '2010-06-01' AND '2010-06-30' and c1.Denominazione ='Basi Di Dati 1'
-AND EXISTS (SELECT * FROM Esami e2 JOIN Corsi c2 ON e2.Corso=c2.Id WHERE e2.Voto>=18 AND e2.Data BETWEEN '2010-06-01' AND '2010-06-30' and c2.Denominazione ='Interfacce Grafiche' and e1.Studente = e2.Studente)
+WHERE e1.Voto>=18
+AND e1.Data BETWEEN '2010-06-01' AND '2010-06-30'
+AND c1.Denominazione ='Basi Di Dati 1'
+AND EXISTS (SELECT *
+			FROM Esami e2
+			JOIN Corsi c2 ON e2.Corso=c2.Id
+			WHERE e2.Voto>=18
+			AND e2.Data BETWEEN '2010-06-01' AND '2010-06-30'
+			AND c2.Denominazione ='Interfacce Grafiche'
+			AND e1.Studente = e2.Studente)
 
 -- VERSIONE 2
 SELECT DISTINCT e1.Studente
 FROM Esami e1
-JOIN Esami e2 ON e1.Studente = e2.Studente and e1.Corso<>e2.Corso
+JOIN Esami e2 ON e1.Studente = e2.Studente and e1.Corso <> e2.Corso
 JOIN Corsi c1 ON e1.Corso=c1.Id
 JOIN Corsi c2 ON e2.Corso=c2.id
 WHERE e1.Voto>=18
-AND e1.Data BETWEEN '2010-06-01' AND '2010-06-30'
-AND e2.Voto>=18
-AND e2.Data BETWEEN '2010-06-01' AND '2010-06-30'
-AND c1.Denominazione = 'Basi Di Dati 1'
-AND c2.Denominazione='Interfacce Grafiche'
+	AND e1.Data BETWEEN '2010-06-01' AND '2010-06-30'
+	AND e2.Voto>=18
+	AND e2.Data BETWEEN '2010-06-01' AND '2010-06-30'
+	AND c1.Denominazione = 'Basi Di Dati 1'
+	AND c2.Denominazione='Interfacce Grafiche'
 
 /**** 2. la matricola degli studenti di informatica che hanno sostenuto basi di dati 1 con votazione superiore alla votazione media (per tale esame); ****/
 SELECT DISTINCT e1.Studente
@@ -31,8 +40,8 @@ FROM Esami e1
 JOIN Corsi c ON c.id=e1.corso
 JOIN CorsiDiLaurea cdl ON cdl.Id=c.CorsoDiLaurea
 WHERE cdl.Denominazione='Informatica'
-AND c.Denominazione = 'Basi Di Dati 1'
-AND e1.voto > (SELECT AVG(e.voto) FROM Esami e WHERE e.Corso=e1.Corso)
+	AND c.Denominazione = 'Basi Di Dati 1'
+	AND e1.voto > (SELECT AVG(e.voto) FROM Esami e WHERE e.Corso=e1.Corso)
 
 /**** 3. i nominativi dei professori che insegnano nel maggior numero di corsi; [nota: non è possibile annidare funzioni di gruppo (es. MAX(COUNT)), ma si può usare  >= ALL invece di MAX] ****/
 SELECT p.Cognome, p.Nome,COUNT(c.Professore) FROM Professori p
@@ -46,8 +55,10 @@ FROM Professori p
 JOIN Corsi c on c.Professore=p.Id
 JOIN Esami e ON e.Corso = c.Id
 GROUP BY p.Cognome,p.Nome, p.Id, c.Denominazione
-HAVING AVG(e.Voto)>=ALL (SELECT AVG(e1.Voto) FROM Esami e1 GROUP BY e1.Corso)
-
+HAVING AVG(e.Voto)>=ALL (
+	SELECT AVG(e1.Voto) FROM Esami e1 GROUP BY e1.Corso
+	)
+	
 /**** 5. per ogni corso, la matricola degli studenti che hanno ottenuto un voto sotto la votazione media del corso, indicando anche corso e voto; ****/
 SELECT e.Studente,e.Corso, e.Voto FROM Esami e 
 WHERE e.Voto < (SELECT AVG(e2.Voto FROM Esami e2) WHERE e2.Corso = e.Corso)
@@ -59,11 +70,10 @@ JOIN Studenti s1 ON s1.Matricola = e1.Studente
 JOIN Professori p1 ON p1.Id = s1.Relatore
 GROUP BY p1.Nome, p1.Cognome, s1.Nome, s1.Cognome,s1.Relatore
 HAVING AVG(e1.Voto)>= ALL (SELECT AVG(e.Voto)
-FROM Esami e 
-JOIN Studenti s ON s.Matricola = e.Studente
-WHERE s.Relatore=s1.Relatore
-GROUP BY s.Cognome, s.Nome,s.Relatore)
-ORDER BY p1.Nome, p1.Cognome
+	FROM Esami e 
+	JOIN Studenti s ON s.Matricola = e.Studente
+	WHERE s.Relatore=s1.Relatore
+	GROUP BY s.Cognome, s.Nome,s.Relatore)
 
 /**** 7. per ogni docente, i corsi correntemente attivati in cui ha attribuito una votazione media superiore alla votazione media assegnata da tale docente (indipendentemente dal corso); ****/
 SELECT p1.Nome, p1.Cognome, c1.id AS corso
@@ -71,17 +81,28 @@ FROM Professori p1
 JOIN Corsi c1 ON c1.Professore=p1.Id
 JOIN Esami e1 ON e1.Corso=c1.Id
 WHERE c1.Attivato IS TRUE
-
 GROUP BY p1.Cognome, p1.Nome, c1.id,p1.Id
-HAVING AVG(e1.voto)> (SELECT AVG(e.Voto) FROM Professori p
-JOIN Corsi c ON c.Professore = p.Id
-JOIN Esami e ON e.Corso=c.id
-WHERE c.Attivato IS TRUE and p1.Id=p.Id)
+HAVING AVG(e1.voto)> (
+	SELECT AVG(e.Voto) FROM Professori p
+	JOIN Corsi c ON c.Professore = p.Id
+	JOIN Esami e ON e.Corso=c.id
+	WHERE c.Attivato IS TRUE
+	AND p1.Id=p.Id)
 
 /**** (*) [divisione] gli studenti non ancora in tesi che hanno passato tutti gli esami del proprio corso di laurea. ****/
 /*** Seleziono in maniera distinta gli studenti che sono senza relatore. Prendo tutti i corsi del corso di laurea dello studente della prima query e verifico che non esistano esami per quello studente, con voto superiore a 18 e per lo stesso corso
 ****/
 SELECT DISTINCT s.Matricola
 FROM Studenti s
-WHERE s.Relatore IS NULL AND NOT EXISTS(
-SELECT * FROM Corsi c1 WHERE c1.CorsoDiLaurea=s.CorsoDiLaurea AND NOT EXISTS (SELECT * FROM Esami e1 WHERE e1.Studente=s.Matricola AND e1.Corso=c1.Id AND e1.Voto>=18))
+WHERE s.Relatore IS NULL
+	AND NOT EXISTS (
+		SELECT *
+		FROM Corsi c1
+		WHERE c1.CorsoDiLaurea = s.CorsoDiLaurea
+			AND NOT EXISTS (
+				SELECT * FROM Esami e1
+				WHERE e1.Studente=s.Matricola
+					AND e1.Corso=c1.Id
+					AND e1.Voto>=18
+				)
+		)
