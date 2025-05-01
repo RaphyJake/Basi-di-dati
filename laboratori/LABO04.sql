52c48edb60d5dcdca3bf110c978e53bd8154f54c
echosr5 <95142130+echosr5@users.noreply.github.com>
2025-05-01 11:58:42 +0200

    Update LABO04.sql

--- laboratori/LABO04.sql
+++ laboratori/LABO04.sql
@@ -141,3 +141,107 @@ WHERE s.relatore IS NULL
           AND e.voto >= 18
       )
   );
+  
+  
+--ESERCIZI SUPPLEMENTARI
+/**
+la matricola degli studenti di informatica 
+che nel mese di giugno 2010 hanno registrato voti per il corso di basi di dati 1 
+ma non per quello di interfacce grafiche - formulare senza usare operatori insiemistici [Formulare 
+l'interrogazione senza usare operatori insiemistici, 
+usando NOT EXISTS e sottointerrogazione correlata];
+*/
+set search_path to "unicorsi";
+select s.matricola
+from studenti s inner join esami e on e.studente = s.matricola
+				inner join corsi c on c.id = e.corso
+where c.denominazione = 'Basi Di Dati 1'
+and extract(YEAR FROM e.data) = 2010
+and extract(MONTH FROM e.data) = 6
+and not exists (select *
+				from esami e1 inner join corsi c1 on c1.id = e1.corso
+				where c1.denominazione = 'Interfacce Grafiche' 
+				and extract(YEAR FROM e1.data) = 2010
+				and extract(MONTH FROM e1.data) = 6
+				and s.matricola = e1.studente);				
+--la matricola dello studente di informatica che ha la votazione media più alta;
+select s.matricola, avg(e.voto)
+from studenti s inner join corsidilaurea cdl on s.corsodilaurea = cdl.id
+				inner join esami e on e.studente = s.matricola
+where cdl.denominazione = 'Informatica'
+group by s.matricola
+having avg(e.voto) >= ALL(SELECT avg(e1.voto)
+						 from studenti s1 inner join esami e1 on e1.studente = s1.matricola
+						 inner join corsidilaurea cdl1 on s1.corsodilaurea = cdl1.id
+						 where cdl1.denominazione = 'Informatica'
+						 group by s1.matricola);
+
+/**
+per ogni corso di informatica, 
+di cui siano stati registrati almeno 2 esami con votazione superiore a 27, 
+la votazione minima,
+media e massima conseguita in tale corso,
+insieme al nome del corso e al numero di esami registrati;
+*/
+ 
+select c.denominazione,min(e.voto) as votoMinimoEsame,max(e.voto) as votoMaxEsame
+from corsi c inner join corsidilaurea cdl on c.corsodilaurea = cdl.id
+inner join esami e on e.corso = c.id
+where cdl.denominazione = 'Informatica' 
+and attivato = true
+group by c.denominazione
+having c.denominazione in (
+
+--Prendo esami con votazione superiore a 27 (almeno 2)
+select c.denominazione
+from esami e inner join corsi c on c.id = e.corso
+where voto >= 27
+group by c.denominazione
+having count(e.voto) >= 2);
+
+--i corsi in cui si ha il maggior numero di studenti con voti insufficienti;
+
+select c.denominazione, count(e1.voto)
+from corsi c inner join esami e1 on e1.corso = c.id
+WHERE e1.voto < 18
+group by c.denominazione
+having count(e1.voto) >= ALL(
+select count(e.voto)
+from esami e 
+where e.voto < 18
+group by e.corso);
+
+/**
+i corsi il cui professore titolare ́e relatore di qualche studente
+di un corso di laurea diverso da quello del corso; 
+[è possibile formularla senza sottointerrogazioni, 
+provate però a usare una sottointerrogazione per esercitarvi]
+*/
+
+select distinct(c.denominazione)
+from corsi c 
+inner join professori p on p.id = c.professore
+inner join studenti s on s.relatore = p.id
+where c.corsodilaurea != s.corsodilaurea
+and exists (
+    select 1
+    from corsi c1 
+    where c1.professore = p.id 
+    and c1.id != c.id
+);
+
+
+/**
+	per ogni corso, il cognome e il nome degli studenti 
+	che hanno ottenuto un voto sotto la votazione media del corso, 
+	indicando anche denominazione del corso e voto.
+*/
+
+select s.nome || ' ' || s.cognome as nomestud, e.voto
+from corsi c inner join esami e on e.corso = c.id
+inner join studenti s on s.matricola = e.studente
+where e.voto < (SELECT avg(e1.voto) 
+					from esami e1 inner join corsi c1 on c1.id = e1.corso
+					where c1.id = c.id
+					group by c1.id);
+
